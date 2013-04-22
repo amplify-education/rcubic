896a94c62ea1735c40c7116f98ac1984cb01cfe6
Hugh Brown <hbrown@wgen.net>
2013-04-22 16:14:31 -0400

    setup.py/.travis.yml/requirements files added

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 
 # This file is part of RCubic
 #
@@ -22,69 +22,79 @@
 #OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 #THE SOFTWARE.
 
+from setuptools import setup
 
-from distutils.core import setup
-from distutils.command.install import INSTALL_SCHEMES
-for scheme in INSTALL_SCHEMES.values():
-    scheme['data'] = scheme['purelib']
+#from distutils.core import setup
+#from distutils.command.install import INSTALL_SCHEMES
+#for scheme in INSTALL_SCHEMES.values():
+#   scheme['data'] = scheme['purelib']
 
-setup(name = 'RCubic',
-      version = '1.1',
-      description = 'RCubic',
-      # Required packages
-      requires = ['MiniREST', 'lxml', 'simplejson', 'pydot', 'gevent'],
-      # List what we provide and obolete for updates
-      provides = ['RCubic'],
-      obsoletes = ['RCubic'],
-      # Main packages
-      packages = ['RCubic'],
-      # Command line scripts
-      scripts = [
-          'bin/rcubic', 'bin/rcubic-cli', 'bin/rcubic-checkin','bin/rcubic-migratedb'
-          ],
-      # Config files
-      data_files = [
-          ('RCubic/', [ "RCubic/rcubic.xml.template" ]),
-          ('RCubic/web/', [ "RCubic/web/index.html" ]),
-          ('RCubic/web/archive/', ["RCubic/web/archive/index.html"]),
-          ('RCubic/web/', [
-              "RCubic/web/index.html",
-              "RCubic/web/archive.html"
-              ]),
-          ('RCubic/web/css/', [
-            "RCubic/web/css/jquery.qtip.min.css",
-            "RCubic/web/css/select2.css",
-            "RCubic/web/css/select2.png",
-            "RCubic/web/css/select2x2.png",
-            "RCubic/web/css/spinner.gif"]),
-          ('RCubic/web/css/syntax/', [
-            "RCubic/web/css/syntax/shCore.css",
-            "RCubic/web/css/syntax/shCoreFadeToGrey.css"]),
-          ('RCubic/web/js/', [
-            "RCubic/web/js/select2.js",
-            "RCubic/web/js/jquery.min.js",
-            "RCubic/web/js/jquery.ui.min.js",
-            "RCubic/web/js/jquery.qtip.min.js"]),
-          ('RCubic/web/js/syntax/', [
-            "RCubic/web/js/syntax/shCore.js",
-            "RCubic/web/js/syntax/shBrushBash.js"]),
-          ('RCubic/web/css/vader/', [
-            "RCubic/web/css/vader/jquery-ui-1.8.21.custom.css" ]),
-          ('RCubic/web/css/vader/images/', [
-            "RCubic/web/css/vader/images/ui-bg_glass_95_fef1ec_1x400.png",
-            "RCubic/web/css/vader/images/ui-icons_aaaaaa_256x240.png",
-            "RCubic/web/css/vader/images/ui-icons_cccccc_256x240.png",
-            "RCubic/web/css/vader/images/ui-icons_bbbbbb_256x240.png",
-            "RCubic/web/css/vader/images/ui-bg_flat_0_aaaaaa_40x100.png",
-            "RCubic/web/css/vader/images/ui-bg_inset-soft_15_121212_1x100.png",
-            "RCubic/web/css/vader/images/ui-bg_highlight-hard_15_888888_1x100.png",
-            "RCubic/web/css/vader/images/ui-icons_c98000_256x240.png",
-            "RCubic/web/css/vader/images/ui-icons_666666_256x240.png",
-            "RCubic/web/css/vader/images/ui-icons_f29a00_256x240.png",
-            "RCubic/web/css/vader/images/ui-bg_highlight-soft_35_adadad_1x100.png",
-            "RCubic/web/css/vader/images/ui-bg_highlight-soft_60_dddddd_1x100.png",
-            "RCubic/web/css/vader/images/ui-icons_cd0a0a_256x240.png",
-            "RCubic/web/css/vader/images/ui-bg_gloss-wave_16_121212_500x100.png",
-            "RCubic/web/css/vader/images/ui-bg_highlight-hard_55_555555_1x100.png"]),
-        ]
-    )
+def reqs_from_file(filename):
+    with open(filename) as f:
+        lineiter = (line.rstrip() for line in f)
+        return filter(None, lineiter)
+
+
+setup(
+    name = 'RCubic',
+    version = '1.1',
+    description = 'RCubic',
+    # Required packages
+    #requires = ['MiniREST', 'lxml', 'simplejson', 'pydot', 'gevent'],
+    install_requires = reqs_from_file('requirements.txt'),
+    tests_require = reqs_from_file('test-requirements.txt'),
+    # List what we provide and obolete for updates
+    provides = ['RCubic'],
+    obsoletes = ['RCubic'],
+    # Main packages
+    packages = ['RCubic'],
+    # Command line scripts
+    scripts = [
+        'bin/rcubic', 'bin/rcubic-cli', 'bin/rcubic-checkin','bin/rcubic-migratedb'
+        ],
+    # Config files
+    data_files = [
+        ('RCubic/', [ "RCubic/rcubic.xml.template" ]),
+        ('RCubic/web/', [ "RCubic/web/index.html" ]),
+        ('RCubic/web/archive/', ["RCubic/web/archive/index.html"]),
+        ('RCubic/web/', [
+            "RCubic/web/index.html",
+            "RCubic/web/archive.html"
+            ]),
+        ('RCubic/web/css/', [
+        "RCubic/web/css/jquery.qtip.min.css",
+        "RCubic/web/css/select2.css",
+        "RCubic/web/css/select2.png",
+        "RCubic/web/css/select2x2.png",
+        "RCubic/web/css/spinner.gif"]),
+        ('RCubic/web/css/syntax/', [
+        "RCubic/web/css/syntax/shCore.css",
+        "RCubic/web/css/syntax/shCoreFadeToGrey.css"]),
+        ('RCubic/web/js/', [
+        "RCubic/web/js/select2.js",
+        "RCubic/web/js/jquery.min.js",
+        "RCubic/web/js/jquery.ui.min.js",
+        "RCubic/web/js/jquery.qtip.min.js"]),
+        ('RCubic/web/js/syntax/', [
+        "RCubic/web/js/syntax/shCore.js",
+        "RCubic/web/js/syntax/shBrushBash.js"]),
+        ('RCubic/web/css/vader/', [
+        "RCubic/web/css/vader/jquery-ui-1.8.21.custom.css" ]),
+        ('RCubic/web/css/vader/images/', [
+        "RCubic/web/css/vader/images/ui-bg_glass_95_fef1ec_1x400.png",
+        "RCubic/web/css/vader/images/ui-icons_aaaaaa_256x240.png",
+        "RCubic/web/css/vader/images/ui-icons_cccccc_256x240.png",
+        "RCubic/web/css/vader/images/ui-icons_bbbbbb_256x240.png",
+        "RCubic/web/css/vader/images/ui-bg_flat_0_aaaaaa_40x100.png",
+        "RCubic/web/css/vader/images/ui-bg_inset-soft_15_121212_1x100.png",
+        "RCubic/web/css/vader/images/ui-bg_highlight-hard_15_888888_1x100.png",
+        "RCubic/web/css/vader/images/ui-icons_c98000_256x240.png",
+        "RCubic/web/css/vader/images/ui-icons_666666_256x240.png",
+        "RCubic/web/css/vader/images/ui-icons_f29a00_256x240.png",
+        "RCubic/web/css/vader/images/ui-bg_highlight-soft_35_adadad_1x100.png",
+        "RCubic/web/css/vader/images/ui-bg_highlight-soft_60_dddddd_1x100.png",
+        "RCubic/web/css/vader/images/ui-icons_cd0a0a_256x240.png",
+        "RCubic/web/css/vader/images/ui-bg_gloss-wave_16_121212_500x100.png",
+        "RCubic/web/css/vader/images/ui-bg_highlight-hard_55_555555_1x100.png"]),
+    ]
+)
