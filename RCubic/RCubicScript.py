5dd7b74bb07209c9c5036207349ebe896f0f036a
Hugh Brown <hbrown@wgen.net>
2013-04-24 11:59:42 -0400

    pep8: fix indentation issues inside functions

--- RCubic/RCubicScript.py
+++ RCubic/RCubicScript.py
@@ -192,22 +192,22 @@ class RCubicScriptParser(object):
                 filepath = "{0}/{1}".format(groupdir, filename)
                 if filename.startswith("{0}_".format(group)):
                     rs = RCubicScript(
-                            filepath,
-                            group.version,
-                            override,
-                            group.phase,
-                            self.logdir,
-                            self.whitelist,
-                            self.blacklist,
-                            self.regexval,
-                            group,
+                        filepath,
+                        group.version,
+                        override,
+                        group.phase,
+                        self.logdir,
+                        self.whitelist,
+                        self.blacklist,
+                        self.regexval,
+                        group,
                     )
                     group.add_script(rs, override)
                 else:
                     logging.debug(
-                                    "Skipping {0}/{1}, does not start with {2}_."
-                                    .format(filepath, group)
-                            )
+                        "Skipping {0}/{1}, does not start with {2}_."
+                        .format(filepath, group)
+                    )
 
     def _glob_expand(self, deps):
         rval = []
@@ -242,7 +242,7 @@ class RCubicScriptParser(object):
         logging.debug("set hrefs")
         for script in self.scripts():
             script.href = "{0}/gitweb?p={1};a=blob;f={2};hb={3}".format(
-                    gerrit, project, script.path[len(repopath)+1:], githash
+                gerrit, project, script.path[len(repopath)+1:], githash
             )
 
     def init_tree(self):
@@ -257,8 +257,8 @@ class RCubicScriptParser(object):
                 tree.cwd = self.workdir
                 tree.name = script.name
                 tree.iterator = exectree.ExecIter(
-                        "{0}_iter".format(script.name),
-                        self.eval_args(script)
+                    "{0}_iter".format(script.name),
+                    self.eval_args(script)
                 )
                 self.subtrees[script.name] = tree
 
@@ -269,11 +269,11 @@ class RCubicScriptParser(object):
         # Initialize jobs and add to trees
         for script in self.scripts():
             script.job = exectree.ExecJob(
-                    script.name,
-                    script.path,
-                    logfile=script.logfile,
-                    arguments=[script.version],
-                    href=script.href
+                script.name,
+                script.path,
+                logfile=script.logfile,
+                arguments=[script.version],
+                href=script.href
             )
             if script.name in self.subtrees:
                 script.job.jobpath = None
@@ -295,8 +295,8 @@ class RCubicScriptParser(object):
         # Check for undefined resources
         if len(self.unusedresources) > 0:
             logging.warning(
-                    "Resources referenced but not defined: {0}."
-                    .format(", ".join(self.unusedresources))
+                "Resources referenced but not defined: {0}."
+                .format(", ".join(self.unusedresources))
             )
 
         # Initialize and set up dependencies
