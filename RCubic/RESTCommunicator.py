3fec4e7e0ab75a2785150435c8d57acb6856a97d
pep8 <pep8@example.com>
2013-04-26 16:51:13 -0400

    ./RCubic/RESTCommunicator.py: indentation contains tabs
    Change-Id: If509d397ca4efc9cd8766514db0ab18adecc76e1

--- RCubic/RESTCommunicator.py
+++ RCubic/RESTCommunicator.py
@@ -30,108 +30,108 @@ from MiniREST.RESTServer import RESTServer, responseCodes, responseTypes
 import logging
 
 class RESTCommunicator(RESTServer):
-	"""RESTCommunicator - creates a new RESTCommunicator instance.
-	Extends RESTServer with custom functions.
+    """RESTCommunicator - creates a new RESTCommunicator instance.
+    Extends RESTServer with custom functions.
 
-	"""
+    """
 
-	def __init__(self, rcubic, bind='0.0.0.0', port=8002, *args, **kwargs):
-		"""Create a RESTCommunicator. Call 'start' to start the server.
+    def __init__(self, rcubic, bind='0.0.0.0', port=8002, *args, **kwargs):
+        """Create a RESTCommunicator. Call 'start' to start the server.
 
-		Keyword arguments:
-		bind -- the address to which the server binds (default '0.0.0.0')
-		port -- the port on which the server listens (default 8002)
-		portRange -- choose first available port to listen on
+        Keyword arguments:
+        bind -- the address to which the server binds (default '0.0.0.0')
+        port -- the port on which the server listens (default 8002)
+        portRange -- choose first available port to listen on
 
-		"""
-		super(RESTCommunicator, self).__init__(bind, port, *args, **kwargs)
-		self.registerFunction('progress', self._progress, token=True)
-		self.registerFunction('reclone', self._reclone, token=True)
-		self.registerFunction('cancel', self._cancel, token=True)
-		self.registerFunction('reschedule', self._reschedule, token=True)
-		self.registerFunction('manualOverride', self._manualOverride, token=True)
-		self.registerFunction('supported', self._supported, token=True)
-		self.features = ['progress', 'reclone', 'reschedule', 'manualOverride', 'cancel']
-		self.rcubic = rcubic
+        """
+        super(RESTCommunicator, self).__init__(bind, port, *args, **kwargs)
+        self.registerFunction('progress', self._progress, token=True)
+        self.registerFunction('reclone', self._reclone, token=True)
+        self.registerFunction('cancel', self._cancel, token=True)
+        self.registerFunction('reschedule', self._reschedule, token=True)
+        self.registerFunction('manualOverride', self._manualOverride, token=True)
+        self.registerFunction('supported', self._supported, token=True)
+        self.features = ['progress', 'reclone', 'reschedule', 'manualOverride', 'cancel']
+        self.rcubic = rcubic
 
-	def _progress(self, env, start_response, post):
-		"""Reponds to a 'progress' request and calls rcubic._updateProgress(..)
+    def _progress(self, env, start_response, post):
+        """Reponds to a 'progress' request and calls rcubic._updateProgress(..)
 
-		Keyword arguments:
-		env -- expects a 'data' list TODO: paramaters
+        Keyword arguments:
+        env -- expects a 'data' list TODO: paramaters
 
-		"""
-		logging.debug("Received Progress report for %s: %s" %(post['scriptName'], post['message']))
-		resp = self.rcubic.updateProgress(post['scriptName'], post['message'])
-		start_response(responseCodes[200], responseTypes['plaintext'])
-		return str(resp)
+        """
+        logging.debug("Received Progress report for %s: %s" %(post['scriptName'], post['message']))
+        resp = self.rcubic.updateProgress(post['scriptName'], post['message'])
+        start_response(responseCodes[200], responseTypes['plaintext'])
+        return str(resp)
 
-	def _reclone(self, env, start_response, post):
-		"""Responds to a 'reclone' request and calls rcubic._initGit()
+    def _reclone(self, env, start_response, post):
+        """Responds to a 'reclone' request and calls rcubic._initGit()
 
-		Keyword arguments:
-		env -- doesn't expect any paramaters
+        Keyword arguments:
+        env -- doesn't expect any paramaters
 
-		"""
-		logging.info("Received reclone request")
-		resp = self.rcubic._initGit()
-		start_response(responseCodes[200], responseTypes['plaintext'])
-		return str(resp)
+        """
+        logging.info("Received reclone request")
+        resp = self.rcubic._initGit()
+        start_response(responseCodes[200], responseTypes['plaintext'])
+        return str(resp)
 
-	def _reschedule(self, env, start_response, post):
-		"""Reponds to a 'reschedule' request and calls rcubic.reschedule(scriptName)
+    def _reschedule(self, env, start_response, post):
+        """Reponds to a 'reschedule' request and calls rcubic.reschedule(scriptName)
 
-		Keyword argument:
-		env -- expects a 'scriptName'
+        Keyword argument:
+        env -- expects a 'scriptName'
 
-		"""
-		scriptName = post['scriptName']
-		logging.info("Received reschedule request for %s." % scriptName)
-		resp = self.rcubic.reschedule(scriptName)
-		start_response(responseCodes[200], responseTypes['plaintext'])
-		if not resp:
-			logging.warning("Reschedule request for %s failed." % post['scriptName'])
-			return str(False)
-		return str(True)
+        """
+        scriptName = post['scriptName']
+        logging.info("Received reschedule request for %s." % scriptName)
+        resp = self.rcubic.reschedule(scriptName)
+        start_response(responseCodes[200], responseTypes['plaintext'])
+        if not resp:
+            logging.warning("Reschedule request for %s failed." % post['scriptName'])
+            return str(False)
+        return str(True)
 
-	def _manualOverride(self, env, start_response, post):
-		"""Responds to a 'manualOverride' request and calls rcubic.manualOverride(scriptName)
+    def _manualOverride(self, env, start_response, post):
+        """Responds to a 'manualOverride' request and calls rcubic.manualOverride(scriptName)
 
-		Keyword argument:
-		env -- expects a scriptName
+        Keyword argument:
+        env -- expects a scriptName
 
-		"""
-		scriptName = post['scriptName']
-		logging.info("Received override request for %s." % scriptName)
-		resp = self.rcubic.manualOverride(scriptName)
-		start_response(responseCodes[200], responseTypes['plaintext'])
-		if not resp:
-			logging.warning("Override request for %s failed." % scriptName)
-			return str(False)
-		return str(True)
+        """
+        scriptName = post['scriptName']
+        logging.info("Received override request for %s." % scriptName)
+        resp = self.rcubic.manualOverride(scriptName)
+        start_response(responseCodes[200], responseTypes['plaintext'])
+        if not resp:
+            logging.warning("Override request for %s failed." % scriptName)
+            return str(False)
+        return str(True)
 
-	def _supported(self, env, start_response, post):
-		"""Responds to a requested asking if a feature is supported
+    def _supported(self, env, start_response, post):
+        """Responds to a requested asking if a feature is supported
 
-		Keyword argument:
-		env -- expects a 'feature'
+        Keyword argument:
+        env -- expects a 'feature'
 
-		"""
-		feature = post['feature']
-		start_response(responseCodes[200], responseTypes['plaintext'])
-		if feature in self.features:
-			return str(True)
-		else:
-			return str(False)
+        """
+        feature = post['feature']
+        start_response(responseCodes[200], responseTypes['plaintext'])
+        if feature in self.features:
+            return str(True)
+        else:
+            return str(False)
 
-	def _cancel(self, env, start_response, post):
-		"""Responds to a 'cancel' request and calls rcubic.abort()
+    def _cancel(self, env, start_response, post):
+        """Responds to a 'cancel' request and calls rcubic.abort()
 
-		Keyword arguments:
-		env -- doesn't expect any paramaters
+        Keyword arguments:
+        env -- doesn't expect any paramaters
 
-		"""
-		logging.info("Received cancel request")
-		resp = self.rcubic.abort()
-		start_response(responseCodes[200], responseTypes['plaintext'])
-		return str(resp)
+        """
+        logging.info("Received cancel request")
+        resp = self.rcubic.abort()
+        start_response(responseCodes[200], responseTypes['plaintext'])
+        return str(resp)
