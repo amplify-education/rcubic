9c94d966f899151d05994f937440ff20d72c16b4
Hugh Brown <hbrown@wgen.net>
2013-04-27 09:16:52 -0400

    hoist shared state to variable

--- RCubic/BotClient.py
+++ RCubic/BotClient.py
@@ -27,7 +27,6 @@ from gevent import event
 
 
 class BotClient(RESTClient):
-
     """Extends the RESTClient class to provide an interface to a BotServer
 
     """
@@ -76,13 +75,20 @@ class BotClient(RESTClient):
 
         """
         events = []
+        data = {
+            "checkInName": checkInName,
+            "message": message,
+            "room": room,
+            'callbackPort': port,
+            "token": self.token
+        }
 
         # If anyuser can check in for the given room
         if anyuser and room:
             ev = event.Event()
             self.restserver.registerCheckIn(room, checkInName, ev)
             events.append(ev)
-            self.getResponse("requestRoomCheckIn", data={"checkInName": checkInName, "message": message, "room": room, 'callbackPort': port, "token": self.token}, *args, **kwargs)
+            self.getResponse("requestRoomCheckIn", data=data, *args, **kwargs)
         else:
             # Go through all users and parse for pm or room
             for user in users:
@@ -94,9 +100,11 @@ class BotClient(RESTClient):
                     user = user + '@' + server
                 self.restserver.registerCheckIn(user, checkInName, ev)
                 if not room:
-                    self.getResponse("requestUserCheckIn", data={"users": [user], "checkInName": checkInName, "message": message, "room": room, "callbackPort": port, "token": self.token}, *args, **kwargs)
+                    data["users"] = [user]
+                    self.getResponse("requestUserCheckIn", data=data, *args, **kwargs)
             if room:
-                self.getResponse("requestUserCheckIn", data={"users": users, "checkInName": checkInName, "message": message, "room": room, 'callbackPort': port, "token": self.token}, *args, **kwargs)
+                data["users"] = users
+                self.getResponse("requestUserCheckIn", data=data, *args, **kwargs)
         tasks = [gevent.spawn(self.waitForEvent, eve) for eve in events]
         # Wait for all check ins or timeout
         gevent.joinall(tasks, timeout=timeout)
