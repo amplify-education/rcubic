cbce1cbad5ea8934bdcbe03ac36b48ba17c5ce1e
Hugh Brown <hbrown@wgen.net>
2013-04-27 10:45:10 -0400

    exectree.py: idiomatic last item in series

--- RCubic/exectree.py
+++ RCubic/exectree.py
@@ -620,7 +620,7 @@ class ExecIter(object):
         if len(self.args) <= 0:
             return ""
         elif self.run > len(self.args):
-            return self.args[len(self.args) - 1]
+            return self.args[-1]
         return self.args[self.run]
 
 
