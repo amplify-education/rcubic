77b7e4ba7a221b0c0c1b57d562df5aa02d7bc6cb
Hugh Brown <hbrown@wgen.net>
2013-04-27 11:29:12 -0400

    RCubicNotification.py: rework indentation

--- RCubic/RCubicNotification.py
+++ RCubic/RCubicNotification.py
@@ -85,9 +85,7 @@ class RCubicNotification(object):
                 )
                 return False
         else:
-            logging.debug("Notification skipped: {0}, {1}, {2}"
-                          .format(groups, subject, message)
-            )
+            logging.debug("Notification skipped: {0}, {1}, {2}".format(groups, subject, message))
             return False
 
     def has_groups(self, groups):
